362db304dc40b535e7e7f8110a46ab47686a5055
Keidensu <sofdissem@gmail.com>
2024-01-20 12:47:39 -0500

    Added random effects + timer to car

---
+++ NattyLightsPounded.gd
@@ -0,0 +1,12 @@
+extends Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	autostart = true
+	wait_time = 5
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
