c44601cfb0c1c7a0343cbef8563592f0c9eea772
Keidensu <sofdissem@gmail.com>
2024-01-20 15:18:07 -0500

    Added DrunkOMeter progress bar

--- drunk-uber-driver-simulator/Scripts/nissan_gtr.gd
+++ drunk-uber-driver-simulator/Scripts/nissan_gtr.gd
@@ -4,6 +4,8 @@ var max_rpm = 1000
 var max_torque = 400
 var rng = RandomNumberGenerator.new()
 var steer_speed = 5
+var bac = 0
+signal onDrink(bac: int)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -14,30 +16,40 @@ func _physics_process(delta):
 	rpm = abs($backright.get_rpm())
 	$backright.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 
-
-func _on_natty_lights_pounded_timeout():
-	var randnum = rng.randi_range(0, 2)
-	print("Random effect " + str(randnum) + " triggered")
-	
-	if randnum == 0:
-		if $backleft.wheel_friction_slip < 1:
-			pass
+func _process(delta):
+	if Input.is_action_just_pressed("drink"):
+		if bac <= 0.4:
+			_add_bac(0.03)
+			var randnum = rng.randi_range(0, 2)
+			print("Random effect " + str(randnum) + " triggered")
+			print("Your bac is now " + str(bac))
+			if randnum == 0:
+				if $backleft.wheel_friction_slip < 1:
+					pass
+				else:
+					$backleft.wheel_friction_slip -= 0.5
+					$backright.wheel_friction_slip -= 0.5
+					$frontleft.wheel_friction_slip -= 0.5
+					$frontright.wheel_friction_slip -= 0.5
+			if randnum == 1:
+				if $backleft.wheel_roll_influence > 1.5:
+					pass
+				else:
+					$backleft.wheel_roll_influence += 0.1
+					$backright.wheel_roll_influence = 0.1
+					$frontleft.wheel_roll_influence = 0.1
+					$frontright.wheel_roll_influence = 0.1
+			if randnum == 2:
+				if steer_speed < 0.1:
+					pass
+				else:
+					steer_speed -= 0.1
 		else:
-			$backleft.wheel_friction_slip -= 0.5
-			$backright.wheel_friction_slip -= 0.5
-			$frontleft.wheel_friction_slip -= 0.5
-			$frontright.wheel_friction_slip -= 0.5
-	if randnum == 1:
-		$backleft.wheel_roll_influence = rng.randf_range(0.0,1.5)
-		$backright.wheel_roll_influence = rng.randf_range(0.0,1.5)
-		$frontleft.wheel_roll_influence = rng.randf_range(0.0,1.5)
-		$frontright.wheel_roll_influence = rng.randf_range(0.0,1.5)
-	if randnum == 2:
-		if steer_speed < 0.1:
-			pass
-		else:
-			steer_speed -= 0.1
+			print("You've had too much to drink.")
 
+func _add_bac(amount: int):
+	bac += amount
+	onDrink.emit(bac)
 
 func _on_body_entered(_body):
 	$"../CollisionSound".play()
