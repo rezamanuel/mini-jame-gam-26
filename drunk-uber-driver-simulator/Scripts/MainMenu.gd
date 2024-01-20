bdbb945dda0ee291f6ec4adc8aa7d381db91f1e2
Evan Andre <evanandre933@gmail.com>
2024-01-20 13:49:51 -0500

    merge main into branch

--- drunk-uber-driver-simulator/Scripts/MainMenu.gd
+++ drunk-uber-driver-simulator/Scripts/MainMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene('')
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
 func _on_controls_button_pressed():
